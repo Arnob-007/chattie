fbc378687f613e5d1b9531f75ef4e963549a2a82
Arnob_007 <Arnob-007@users.noreply.github.com>
2020-10-08 01:33:24 +0600

    App build

---
+++ build/precache-manifest.62623403df89e13719b20856ff9c20df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cf855a310de9a2d08c7e9389f6aa551",
+    "url": "/index.html"
+  },
+  {
+    "revision": "287280803af2cbd655aa",
+    "url": "/static/css/main.098ea786.chunk.css"
+  },
+  {
+    "revision": "794455391b235a99cefb",
+    "url": "/static/js/2.4b6e2b21.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/2.4b6e2b21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "287280803af2cbd655aa",
+    "url": "/static/js/main.98350cef.chunk.js"
+  },
+  {
+    "revision": "24c6be27c4d56bfb9129",
+    "url": "/static/js/runtime-main.ed4880da.js"
+  }
+]);
