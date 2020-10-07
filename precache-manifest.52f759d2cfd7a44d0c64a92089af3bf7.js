b29d32599e2c1128fc0ef9f463d0a2e9db3610e9
Arnob_007 <Arnob-007@users.noreply.github.com>
2020-10-08 01:54:50 +0600

    Updates

---
+++ precache-manifest.52f759d2cfd7a44d0c64a92089af3bf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41a8ff1ca61589b078fc23a5619d85c",
+    "url": "/chattie/index.html"
+  },
+  {
+    "revision": "cdf907d16a016da1926f",
+    "url": "/chattie/static/css/main.098ea786.chunk.css"
+  },
+  {
+    "revision": "780836e47742252c1ead",
+    "url": "/chattie/static/js/2.ca14a1b6.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/chattie/static/js/2.ca14a1b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdf907d16a016da1926f",
+    "url": "/chattie/static/js/main.fd20a32c.chunk.js"
+  },
+  {
+    "revision": "f0412bbb67a7ce69d4e6",
+    "url": "/chattie/static/js/runtime-main.02b2d22b.js"
+  }
+]);
